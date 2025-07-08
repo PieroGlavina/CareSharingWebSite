b0a9d0008f3a2090fc64406c969787213a7973ee
PieroGlavina <1f.glavinapiero@gmail.com>
2025-07-08 23:05:06 +0200

    logo issues fixed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -15,9 +15,8 @@ const Hero = () => {
         <section className="w-full min-h-screen bg-custom-blue flex items-center justify-center py-10 px-4">
             <div className="flex flex-col-reverse lg:flex-row items-center justify-between w-full max-w-7xl mx-auto">
 
-                {/* Left: Text & Buttons */}
                 <div className="flex flex-col items-center text-center gap-5 lg:w-2/5">
-                    <h1 id="textAnim" className="font-preuksa_semibold text-white text-6xl lg:text-9xl opacity-0">CareSharing</h1>
+                    <h1 id="textAnim" className="font-preuksa_semibold text-white text-6xl lg:text-8xl opacity-0">CareSharing</h1>
                     <p id="textAnim" className="text-white font-preuksa_regular text-2xl lg:text-4xl opacity-0">Scopriti capace di amare</p>
 
                     <div id="cta" className="flex flex-col sm:flex-row items-center justify-center gap-5 opacity-0 mt-5">
@@ -37,7 +36,6 @@ const Hero = () => {
                     </div>
                 </div>
 
-                {/* Right: Video */}
                 <div className="w-full lg:w-[100%] mb-10 lg:mb-0 lg:ml-10">
                     <video className="pointer-events-none w-full h-auto rounded-lg" autoPlay muted loop playsInline
                            key={mainVideo}>
